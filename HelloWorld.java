fd33dc74f337f4403a74fcf3a67fa6c8ed3683e6
Ethan Goldfarb <cheesical@gmail.com>
2021-04-14 14:16:40 -0700

    added Ethan info

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! Adam Perlin; 3 years");
+        System.out.println("Hello World! Adam Perlin; 3 years; Ethan Goldfarb, 4 years.");
     }
 }
